d89542fe7981d6292c667e8ba5a8b33577e27002
장승원 <jangseungwon@jangseung-woui-MacBookAir.local>
2026-07-11 17:50:20 +0900

    initial-commit

---
+++ src/test/java/com/example/consistence_control/ConsistenceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consistence_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsistenceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
